aef601a0f0c660945da7b041bfaf90eb1ba4f81a
zoto-ff <minytvaaa@gmail.com>
2023-03-06 23:51:50 +0300

    Update webui-user.sh

--- webui-user.sh
+++ webui-user.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export COMMANDLINE_ARGS="--xformers --enable-insecure-extension-access --cloudflared --share --vae-path /content/stable-diffusion-webui/models/Stable-diffusion/main.vae.pt --no-half-vae --ckpt /content/stable-diffusion-webui/models/Stable-diffusion/model.ckpt --disable-safe-unpickle --disable-console-progressbars --ui-settings-file settings.json --skip-torch-cuda-test"
 export ACCELERATE="True"
+python launch.py
