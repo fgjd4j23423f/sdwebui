954735828463ede244057f1a9fb54476930da178
zoto-ff <minytvaaa@gmail.com>
2023-03-10 06:58:55 +0300

    1

--- scripts/callback.py
+++ scripts/callback.py
@@ -1,20 +1,17 @@
-import math
-import os
-import sys
-import traceback
 import requests
 import io
 import base64
 import urllib3
 import threading
 import time
+import json
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 import modules.scripts as scripts
 import gradio as gr
 
 from modules.processing import Processed, process_images
-
+from PIL import Image, ImageEnhance
 
 class Script(scripts.Script):
     t = 0
@@ -24,8 +21,9 @@ class Script(scripts.Script):
 
     def ui(self, is_img2img):
         callback_url = gr.Textbox(label="callback_url", lines=1)
+        filters = gr.Textbox(label="filters", lines=1)
 
-        return [callback_url]
+        return [callback_url, filters]
 
     def send(self, image, callback_url, gen_time):
         in_mem_file = io.BytesIO()
@@ -48,12 +46,39 @@ class Script(scripts.Script):
     def postprocess_batch(self, p=None, x_samples_ddim=None, batch_number=None):
         self.t = time.time() - self.t
 
-    def run(self, p, callback_url):
+    def run(self, p, callback_url, filters):
         p.scripts.process_batch = self.process_batch
         p.scripts.postprocess_batch = self.postprocess_batch
 
         proc = process_images(p)
         image = proc.images
+
+        filters = json.loads(filters)
+        for filter in filters:
+            match filter[0]:
+                case 'exposure':
+                    gamma = float(filter[1])
+
+                    height = image.size[0]
+                    width = image.size[1]
+                    result_img1 = Image.new(mode="RGB", size=(height, width), color=0)
+                    for x in range(height):
+                        for y in range(width):
+                            r = pow(image.getpixel((x, y))[0] / 255, (1 / gamma)) * 255
+                            g = pow(image.getpixel((x, y))[1] / 255, (1 / gamma)) * 255
+                            b = pow(image.getpixel((x, y))[2] / 255, (1 / gamma)) * 255
+
+                            color = (int(r), int(g), int(b))
+                            result_img1.putpixel((x, y), color)
+
+                    image.close()
+                    image = result_img1
+
+                case 'saturation':
+                    multiplier = float(filter[1])
+
+                    converter = ImageEnhance.Color(image)
+                    image = converter.enhance(multiplier)
 
         t = threading.Thread(target=self.send, args=(image[0], callback_url, self.t))
         t.daemon = True
